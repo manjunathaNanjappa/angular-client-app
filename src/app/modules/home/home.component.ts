fa18971e8e09fd411b65c4dbd976861f7a8fc1ef
Manjunatha N <manjunatha.n@tandemloop.in>
2023-11-22 23:23:32 +0530

    updated pic download & export data

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NzUploadFile, NzUploadType } from 'ng-zorro-antd/upload';
 import { ApplicationService } from 'src/app/services/application-service/application.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
-import { saveAs } from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-home',
@@ -28,6 +23,10 @@ export class HomeComponent implements OnInit {
   profilePicFileList: NzUploadFile[] = [];
   marksSheetFileList: NzUploadFile[] = [];
   isEditApplication: boolean = false;
+
+  checked = false;
+  indeterminate = false;
+  setOfCheckedId = new Set<number>();
 
   applicationModal: any;
 
@@ -175,18 +174,65 @@ export class HomeComponent implements OnInit {
   }
 
   downloadFile(fileData: any) {
-    let base64 = fileData.file.bufferData.toString('base64');
-    console.log('base64', base64);
-    const blob = new Blob([fileData.file], { type: fileData.mimeType });
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = fileData.fileName;
-    link.click();
+    console.log('fileData', fileData);
+    const downloadLink = document.createElement('a');
+    downloadLink.href = fileData.file;
+    downloadLink.download = fileData.fileName;
+    downloadLink.click();
   }
 
   editApplicationDetails(applicationData: any) {
     console.log('applicationData', applicationData);
     this.isEditApplication = true;
     this.openApplicationDrawer();
+  }
+
+  refreshCheckedStatus(): void {
+    this.checked = this.listOfApplications.every((item) =>
+      this.setOfCheckedId.has(item._id)
+    );
+    this.indeterminate =
+      this.listOfApplications.some((item) =>
+        this.setOfCheckedId.has(item._id)
+      ) && !this.checked;
+  }
+
+  onAllChecked(value: boolean): void {
+    this.listOfApplications.forEach((item) =>
+      this.updateCheckedSet(item._id, value)
+    );
+    this.refreshCheckedStatus();
+  }
+
+  updateCheckedSet(id: any, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  onItemChecked(id: any, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onClickExport() {
+    let selectedApplications = this.listOfApplications.filter((application) => {
+      if (this.setOfCheckedId.has(application._id)) {
+        delete application.profilePic;
+        delete application.marksSheet;
+        delete application.createdBy;
+        delete application.createdAt;
+        return application;
+      }
+    });
+    let timeSpan = new Date().toString();
+    let fileName = `Applications-${timeSpan}`;
+
+    let workBook = XLSX.utils.book_new();
+    let workSheet = XLSX.utils.json_to_sheet(selectedApplications);
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'applications');
+    XLSX.writeFile(workBook, `${fileName}.xlsx`);
   }
 }
